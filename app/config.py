dc048280c31e9d169b8930b0097a260fe518db10
Sukunadot <85873221+Sukunadot@users.noreply.github.com>
2024-08-11 17:11:05 +1000

    config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DATABASE_URL: str = os.getenv("postgresql://db_224142778_sit722_part1_gssv_user:odZ588gSVTCh9oaV2OqW2w5OEitfGvla@dpg-cqs674ggph6c73a7ikgg-a.singapore-postgres.render.com/db_224142778_sit722_part1_gssv")
 
 settings = Settings()
